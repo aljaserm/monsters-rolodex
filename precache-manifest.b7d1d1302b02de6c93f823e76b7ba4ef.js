37ae2b811ed0beb96f99efc9d3d12586de60912d
Mohammed alJaser <aljaserm7@gmail.com>
2019-07-27 11:44:48 -0400

    Updates

---
+++ precache-manifest.b7d1d1302b02de6c93f823e76b7ba4ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b74ed3e9682e3255db32dae8d1dc9e6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "797deb9cbddb9acfffd4",
+    "url": "/monsters-rolodex/static/css/main.752c83f7.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "797deb9cbddb9acfffd4",
+    "url": "/monsters-rolodex/static/js/main.9321d949.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
